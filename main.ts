ec0474cf725e8bf032b5d4cf7c2ff2d84fec34ea
Albert Darchiev <144930026+AlbertDarchiev@users.noreply.github.com>
2023-12-12 22:57:44 +0100

    level2

--- main.ts
+++ main.ts
@@ -4,18 +4,25 @@ namespace SpriteKind {
     export const house = SpriteKind.create()
     export const citizen = SpriteKind.create()
 }
+controller.right.onEvent(ControllerButtonEvent.Released, function () {
+    char_ninja.vx = 0
+})
+controller.left.onEvent(ControllerButtonEvent.Released, function () {
+    char_ninja.vx = 0
+})
 function death_char () {
     controller.moveSprite(char_ninja, 0, 0)
     is_alive = false
+    if (current_level == 1) {
+        level1()
+    } else if (current_level == 2) {
+        level2()
+    } else {
+    	
+    }
     sprites.destroyAllSpritesOfKind(SpriteKind.prisioner)
-    level1()
     restore_objects()
 }
-controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
-    if (char_ninja.isHittingTile(CollisionDirection.Bottom) && is_alive && !(in_main)) {
-        char_ninja.vy = -150
-    }
-})
 function level1 () {
     in_main = false
     is_alive = true
@@ -40,7 +47,7 @@ scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile`, function (sprite, lo
     if (current_level == 1) {
         tiles.placeOnTile(char_ninja, tiles.getTileLocation(3, 3))
     } else if (current_level == 2) {
-    	
+        tiles.placeOnTile(char_ninja, tiles.getTileLocation(9, 8))
     } else {
     	
     }
@@ -106,203 +113,31 @@ function foodhealth () {
         . c c c c c c c . . . . . . . . 
         `]
 }
-controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    if (char_ninja.isHittingTile(CollisionDirection.Bottom) && is_alive && !(in_main)) {
-        char_ninja.vy = -150
-    }
-})
 scene.onOverlapTile(SpriteKind.Player, assets.tile`switchDown`, function (sprite5, location5) {
     tiles.setTileAt(location5, assets.tile`switchUp0`)
     tiles.setWallAt(tiles.getTileLocation(49, 1), false)
     tiles.setTileAt(tiles.getTileLocation(49, 1), assets.tile`transparency16`)
     game.showLongText("Eso debió haber activado algo...", DialogLayout.Top)
 })
-controller.anyButton.onEvent(ControllerButtonEvent.Released, function () {
-    animation.stopAnimation(animation.AnimationTypes.All, char_ninja)
-})
 statusbars.onStatusReached(StatusBarKind.Health, statusbars.StatusComparison.LTE, statusbars.ComparisonType.Percentage, 50, function (status) {
     char_ninja.sayText("Me estoy muriendo!", 2000, false)
 })
-controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
-    animation.runImageAnimation(
-    char_ninja,
-    [img`
-        . . . . f f f f f . . . . . . . 
-        . . . f e e e e e f . . . . . . 
-        . . f d d d d e e e f . . . . . 
-        . c d f d d f d e e f f . . . . 
-        . c d f d d f d e e d d f . . . 
-        c d e e d d d d e e b d c . . . 
-        c d d d d c d d e e b d c . f f 
-        c c c c c d d d e e f c . f e f 
-        . f d d d d d e e f f . . f e f 
-        . . f f f f f e e e e f . f e f 
-        . . . . f e e e e e e e f f e f 
-        . . . f e f f e f e e e e f f . 
-        . . . f e f f e f e e e e f . . 
-        . . . f d b f d b f f e f . . . 
-        . . . f d d c d d b b d f . . . 
-        . . . . f f f f f f f f f . . . 
-        `,img`
-        . . . . f f f f f . . . . . . . 
-        . . . f e e e e e f . . . . . . 
-        . . f d d d d e e e f . . . . . 
-        . c d f d d f d e e f . . . . . 
-        . c d f d d f d e e f f . . . . 
-        c d e e d d d d e e d d f . . . 
-        c d d d d c d d e e b d c . . . 
-        c c c c c d d e e e b d c . f f 
-        . f d d d d e e e f f c . f e f 
-        . f f f f f f e e e e f . f e f 
-        . f f f f e e e e e e e f f e f 
-        f d d f d d f e f e e e e f f . 
-        f d b f d b f e f e e e e f . . 
-        f f f f f f f f f f f f e f . . 
-        . . . . . . . . . f c d d f . . 
-        . . . . . . . . . . f f f f . . 
-        `,img`
-        . . . . f f f f f . . . . . . . 
-        . . . f e e e e e f . . . . . . 
-        . . f d d d d e e e f f . . . . 
-        . c d d d d d d e e d d f . . . 
-        . c d f d d f d e e b d c . . . 
-        c d d f d d f d e e b d c . f f 
-        c d e e d d d d e e f c . f e f 
-        c d d d d c d e e e f . . f e f 
-        . f c c c d e e e f f . . f e f 
-        . . f f f f f e e e e f . f e f 
-        . . . . f e e e e e e e f f f . 
-        . . f f e f e e f e e e e f . . 
-        . f e f f e e f f f e e e f . . 
-        f d d b d d c f f f f f f b f . 
-        f d d c d d d f . . f c d d f . 
-        . f f f f f f f . . . f f f . . 
-        `,img`
-        . . . . f f f f f . . . . . . . 
-        . . . f e e e e e f f f . . . . 
-        . . f d d d e e e e d d f . . . 
-        . c d d d d d e e e b d c . . . 
-        . c d d d d d d e e b d c . . . 
-        c d d f d d f d e e f c . f f . 
-        c d d f d d f d e e f . . f e f 
-        c d e e d d d d e e f . . f e f 
-        . f d d d c d e e f f . . f e f 
-        . . f f f d e e e e e f . f e f 
-        . . . . f e e e e e e e f f f . 
-        . . . . f f e e e e e b f f . . 
-        . . . f e f f e e c d d f f . . 
-        . . f d d b d d c f f f . . . . 
-        . . f d d c d d d f f . . . . . 
-        . . . f f f f f f f . . . . . . 
-        `,img`
-        . . . . f f f f f . . . . . . . 
-        . . . f e e e e e f . . . . . . 
-        . . f d d d d e e e f . . . . . 
-        . c d f d d f d e e f f . . . . 
-        . c d f d d f d e e d d f . . . 
-        c d e e d d d d e e b d c . . . 
-        c d d d d c d d e e b d c . . . 
-        c c c c c d d e e e f c . . . . 
-        . f d d d d e e e f f . . . . . 
-        . . f f f f f e e e e f . . . . 
-        . . . . f f e e e e e e f . f f 
-        . . . f e e f e e f e e f . e f 
-        . . f e e f e e f e e e f . e f 
-        . f b d f d b f b b f e f f e f 
-        . f d d f d d f d d b e f f f f 
-        . . f f f f f f f f f f f f f . 
-        `],
-    100,
-    true
-    )
+controller.anyButton.onEvent(ControllerButtonEvent.Released, function () {
+    animation.stopAnimation(animation.AnimationTypes.All, char_ninja)
 })
 function load_level (lvl: number) {
     if (lvl == 1) {
         level1()
     } else if (lvl == 2) {
-    	
+        level2()
     } else {
     	
     }
+    sprites.destroyAllSpritesOfKind(SpriteKind.citizen)
     sprites.destroyAllSpritesOfKind(SpriteKind.Enemy)
     sprites.destroyAllSpritesOfKind(SpriteKind.Food)
     create_lava()
 }
-function char_ongame_options () {
-    controller.moveSprite(char_ninja, 50, 0)
-    char_ninja.ay = 300
-    char_ninja.setImage(img`
-        . . . . . . . f f f f f . . . . 
-        . . . . . . f e e e e e f . . . 
-        . . . . . f e e e d d d d f . . 
-        . . . . f f e e d f d d f d c . 
-        . . . f d d e e d f d d f d c . 
-        . . . c d b e e d d d d e e d c 
-        f f . c d b e e d d c d d d d c 
-        f e f . c f e e d d d c c c c c 
-        f e f . . f f e e d d d d d f . 
-        f e f . f e e e e f f f f f . . 
-        f e f f e e e e e e e f . . . . 
-        . f f e e e e f e f f e f . . . 
-        . . f e e e e f e f f e f . . . 
-        . . . f e f f b d f b d f . . . 
-        . . . f d b b d d c d d f . . . 
-        . . . f f f f f f f f f . . . . 
-        `)
-}
-function create_zombies () {
-    for (let index = 0; index < 7; index++) {
-        zombie = sprites.create(img`
-            . . f f f f f f f . . . . . . . 
-            . f f 3 b 3 b 3 f f . . . . . . 
-            f f b b 3 b 3 2 3 f f . . . . . 
-            f 3 b 3 b 2 b 3 b 3 f f . . . . 
-            f b 3 2 3 3 3 b 3 2 3 f . . . . 
-            f 7 7 7 7 2 7 7 7 7 7 f . . . . 
-            f 7 7 7 7 7 2 7 7 e 7 f . . . . 
-            f 7 7 f f 7 7 f f 7 7 f . . . . 
-            f e 7 7 7 7 7 7 7 7 e f . . . . 
-            . f e 7 7 b b 7 7 e f . . . . . 
-            . f f e 7 2 7 7 e f f . . . . . 
-            7 7 f b 1 1 7 1 b f 7 7 . . . . 
-            2 d f 1 1 7 1 1 1 f d 7 . . . . 
-            7 7 f 1 1 1 1 2 1 f 7 7 . . . . 
-            . . . f f f f f f . . . . . . . 
-            . . . f f . . f f . . . . . . . 
-            `, SpriteKind.Enemy)
-        tiles.placeOnRandomTile(zombie, sprites.dungeon.floorDarkDiamond)
-        zombie.follow(char_ninja, randint(0, 10))
-    }
-}
-controller.right.onEvent(ControllerButtonEvent.Released, function () {
-    char_ninja.vx = 0
-})
-controller.left.onEvent(ControllerButtonEvent.Released, function () {
-    char_ninja.vx = 0
-})
-scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.hazardLava1, function (sprite8, location7) {
-    death_char()
-})
-function level2 () {
-    in_main = false
-    is_alive = true
-    sprites.destroyAllSpritesOfKind(SpriteKind.house)
-    scene.cameraFollowSprite(char_ninja)
-    scene.setBackgroundImage(assets.image`black`)
-    tiles.setCurrentTilemap(tilemap`nivel14`)
-    char_ninja.setPosition(30, 30)
-    chest_is_closed = true
-    char_ongame_options()
-}
-scene.onOverlapTile(SpriteKind.Player, assets.tile`house0`, function (sprite32, location32) {
-    current_level = 1
-    char_ninja.setStayInScreen(true)
-    load_level(current_level)
-})
-statusbars.onZero(StatusBarKind.Health, function (status) {
-    char_ninja.startEffect(effects.disintegrate)
-    sprites.destroy(char_ninja)
-})
 controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
     animation.runImageAnimation(
     char_ninja,
@@ -396,6 +231,173 @@ controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
     true
     )
 })
+function char_ongame_options () {
+    controller.moveSprite(char_ninja, 50, 0)
+    char_ninja.ay = 300
+    char_ninja.setImage(img`
+        . . . . . . . f f f f f . . . . 
+        . . . . . . f e e e e e f . . . 
+        . . . . . f e e e d d d d f . . 
+        . . . . f f e e d f d d f d c . 
+        . . . f d d e e d f d d f d c . 
+        . . . c d b e e d d d d e e d c 
+        f f . c d b e e d d c d d d d c 
+        f e f . c f e e d d d c c c c c 
+        f e f . . f f e e d d d d d f . 
+        f e f . f e e e e f f f f f . . 
+        f e f f e e e e e e e f . . . . 
+        . f f e e e e f e f f e f . . . 
+        . . f e e e e f e f f e f . . . 
+        . . . f e f f b d f b d f . . . 
+        . . . f d b b d d c d d f . . . 
+        . . . f f f f f f f f f . . . . 
+        `)
+}
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    animation.runImageAnimation(
+    char_ninja,
+    [img`
+        . . . . f f f f f . . . . . . . 
+        . . . f e e e e e f . . . . . . 
+        . . f d d d d e e e f . . . . . 
+        . c d f d d f d e e f f . . . . 
+        . c d f d d f d e e d d f . . . 
+        c d e e d d d d e e b d c . . . 
+        c d d d d c d d e e b d c . f f 
+        c c c c c d d d e e f c . f e f 
+        . f d d d d d e e f f . . f e f 
+        . . f f f f f e e e e f . f e f 
+        . . . . f e e e e e e e f f e f 
+        . . . f e f f e f e e e e f f . 
+        . . . f e f f e f e e e e f . . 
+        . . . f d b f d b f f e f . . . 
+        . . . f d d c d d b b d f . . . 
+        . . . . f f f f f f f f f . . . 
+        `,img`
+        . . . . f f f f f . . . . . . . 
+        . . . f e e e e e f . . . . . . 
+        . . f d d d d e e e f . . . . . 
+        . c d f d d f d e e f . . . . . 
+        . c d f d d f d e e f f . . . . 
+        c d e e d d d d e e d d f . . . 
+        c d d d d c d d e e b d c . . . 
+        c c c c c d d e e e b d c . f f 
+        . f d d d d e e e f f c . f e f 
+        . f f f f f f e e e e f . f e f 
+        . f f f f e e e e e e e f f e f 
+        f d d f d d f e f e e e e f f . 
+        f d b f d b f e f e e e e f . . 
+        f f f f f f f f f f f f e f . . 
+        . . . . . . . . . f c d d f . . 
+        . . . . . . . . . . f f f f . . 
+        `,img`
+        . . . . f f f f f . . . . . . . 
+        . . . f e e e e e f . . . . . . 
+        . . f d d d d e e e f f . . . . 
+        . c d d d d d d e e d d f . . . 
+        . c d f d d f d e e b d c . . . 
+        c d d f d d f d e e b d c . f f 
+        c d e e d d d d e e f c . f e f 
+        c d d d d c d e e e f . . f e f 
+        . f c c c d e e e f f . . f e f 
+        . . f f f f f e e e e f . f e f 
+        . . . . f e e e e e e e f f f . 
+        . . f f e f e e f e e e e f . . 
+        . f e f f e e f f f e e e f . . 
+        f d d b d d c f f f f f f b f . 
+        f d d c d d d f . . f c d d f . 
+        . f f f f f f f . . . f f f . . 
+        `,img`
+        . . . . f f f f f . . . . . . . 
+        . . . f e e e e e f f f . . . . 
+        . . f d d d e e e e d d f . . . 
+        . c d d d d d e e e b d c . . . 
+        . c d d d d d d e e b d c . . . 
+        c d d f d d f d e e f c . f f . 
+        c d d f d d f d e e f . . f e f 
+        c d e e d d d d e e f . . f e f 
+        . f d d d c d e e f f . . f e f 
+        . . f f f d e e e e e f . f e f 
+        . . . . f e e e e e e e f f f . 
+        . . . . f f e e e e e b f f . . 
+        . . . f e f f e e c d d f f . . 
+        . . f d d b d d c f f f . . . . 
+        . . f d d c d d d f f . . . . . 
+        . . . f f f f f f f . . . . . . 
+        `,img`
+        . . . . f f f f f . . . . . . . 
+        . . . f e e e e e f . . . . . . 
+        . . f d d d d e e e f . . . . . 
+        . c d f d d f d e e f f . . . . 
+        . c d f d d f d e e d d f . . . 
+        c d e e d d d d e e b d c . . . 
+        c d d d d c d d e e b d c . . . 
+        c c c c c d d e e e f c . . . . 
+        . f d d d d e e e f f . . . . . 
+        . . f f f f f e e e e f . . . . 
+        . . . . f f e e e e e e f . f f 
+        . . . f e e f e e f e e f . e f 
+        . . f e e f e e f e e e f . e f 
+        . f b d f d b f b b f e f f e f 
+        . f d d f d d f d d b e f f f f 
+        . . f f f f f f f f f f f f f . 
+        `],
+    100,
+    true
+    )
+})
+function create_zombies () {
+    for (let index = 0; index < 7; index++) {
+        zombie = sprites.create(img`
+            . . f f f f f f f . . . . . . . 
+            . f f 3 b 3 b 3 f f . . . . . . 
+            f f b b 3 b 3 2 3 f f . . . . . 
+            f 3 b 3 b 2 b 3 b 3 f f . . . . 
+            f b 3 2 3 3 3 b 3 2 3 f . . . . 
+            f 7 7 7 7 2 7 7 7 7 7 f . . . . 
+            f 7 7 7 7 7 2 7 7 e 7 f . . . . 
+            f 7 7 f f 7 7 f f 7 7 f . . . . 
+            f e 7 7 7 7 7 7 7 7 e f . . . . 
+            . f e 7 7 b b 7 7 e f . . . . . 
+            . f f e 7 2 7 7 e f f . . . . . 
+            7 7 f b 1 1 7 1 b f 7 7 . . . . 
+            2 d f 1 1 7 1 1 1 f d 7 . . . . 
+            7 7 f 1 1 1 1 2 1 f 7 7 . . . . 
+            . . . f f f f f f . . . . . . . 
+            . . . f f . . f f . . . . . . . 
+            `, SpriteKind.Enemy)
+        tiles.placeOnRandomTile(zombie, sprites.dungeon.floorDarkDiamond)
+        zombie.follow(char_ninja, randint(0, 10))
+    }
+}
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.hazardLava1, function (sprite8, location7) {
+    death_char()
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (char_ninja.isHittingTile(CollisionDirection.Bottom) && is_alive && !(in_main)) {
+        char_ninja.vy = -150
+    }
+})
+function level2 () {
+    in_main = false
+    is_alive = true
+    sprites.destroyAllSpritesOfKind(SpriteKind.house)
+    scene.cameraFollowSprite(char_ninja)
+    scene.setBackgroundImage(assets.image`black2`)
+    tiles.setCurrentTilemap(tilemap`level0`)
+    char_ninja.setPosition(30, 30)
+    chest_is_closed = true
+    char_ongame_options()
+}
+scene.onOverlapTile(SpriteKind.Player, assets.tile`house0`, function (sprite32, location32) {
+    current_level = 1
+    char_ninja.setStayInScreen(true)
+    load_level(current_level)
+})
+statusbars.onZero(StatusBarKind.Health, function (status) {
+    char_ninja.startEffect(effects.disintegrate)
+    sprites.destroy(char_ninja)
+})
 function createCitizen () {
     survivor = sprites.create(img`
         . . . . f f f f . . . . . 
@@ -449,6 +451,11 @@ function load_menu () {
     create_zombies()
     createCitizen()
 }
+controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (char_ninja.isHittingTile(CollisionDirection.Bottom) && is_alive && !(in_main)) {
+        char_ninja.vy = -150
+    }
+})
 scene.onOverlapTile(SpriteKind.Player, assets.tile`lava_enemy`, function (sprite2, location2) {
     death_char()
 })
@@ -470,7 +477,7 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.citizen, function (sprite, other
     game.showLongText("Tienes que ayudarme a rescatar a mis amigos!", DialogLayout.Bottom)
     game.showLongText("Están dentro de las casas, no pueden salir solos", DialogLayout.Bottom)
     game.showLongText("Pero cuidado con los zombies, intentarán matarte", DialogLayout.Bottom)
-    pause(5000)
+    pause(3000)
 })
 scene.onOverlapTile(SpriteKind.Player, assets.tile`char_guide`, function (sprite7, location6) {
     char_ninja.sayText("Selecciona la bajada correcta...", 200, false)
@@ -571,8 +578,8 @@ let statusbar: StatusBarSprite = null
 let survivor: Sprite = null
 let zombie: Sprite = null
 let listfood: Image[] = []
-let current_level = 0
 let chest_is_closed = false
+let current_level = 0
 let is_alive = false
 let char_ninja: Sprite = null
 let in_main = false
@@ -595,5 +602,7 @@ char_ninja = sprites.create(img`
     . f d d f d d f d d b e f f f f 
     . . f f f f f f f f f f f f f . 
     `, SpriteKind.Player)
+tiles.setCurrentTilemap(tilemap`nivel13`)
+tiles.placeOnTile(char_ninja, tiles.getTileLocation(15, 3))
 load_menu()
 create_health()
